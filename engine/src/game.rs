7141632db34cc202357ef5d263d5de2ed01ea77a
odkaz <one.destiny.japan@gmail.com>
2026-04-14 15:11:58 +0200

    add: enum Player and GameStatue

--- engine/src/game.rs
+++ engine/src/game.rs
@@ -1,15 +1,45 @@
 pub type Board = [[u8; 19]; 19];
 
+#[derive(Copy, Clone, PartialEq)]
+pub enum Player {
+    Black,
+    White,
+}
+
+impl Player {
+    pub fn opponent(self) -> Player {
+        match self {
+            Player::Black => Player::White,
+            Player::White => Player::Black,
+        }
+    }
+
+    pub fn to_u8(self) -> u8 {
+        match self {
+            Player::Black => 1,
+            Player::White => 2,
+        }
+    }
+}
+
 pub struct Game {
     pub board: Board,
-    pub current_player: u8,
+    pub current_player: Player,
+    pub status: GameStatus,
+}
+
+pub enum GameStatus {
+    Ongoing,
+    Win(Player),
+    Draw,
 }
 
 impl Game {
     pub fn new() -> Self {
         Self {
             board: [[0; 19]; 19],
-            current_player: 1,
+            current_player: Player::Black,
+            status: GameStatus::Ongoing,
         }
     }
 
@@ -22,9 +52,20 @@ impl Game {
             return Err("Cell already occupied".to_string());
         }
 
-        self.board[y][x] = self.current_player;
+        if !matches!(self.status, GameStatus::Ongoing) {
+            return Err("Game already finished".to_string());
+        }
 
-        self.current_player = if self.current_player == 1 { 2 } else { 1 };
+        self.board[y][x] = self.current_player.to_u8();
+
+        if self.check_win(x, y) {
+            // mark game as finished
+            self.status = GameStatus::Win(self.current_player)
+        } else if self.is_board_full() {
+            self.status = GameStatus::Draw;
+        } else {
+            self.current_player = self.current_player.opponent();
+        }
 
         Ok(())
     }
@@ -35,12 +76,29 @@ impl Game {
                 let symbol = match cell {
                     0 => ".",
                     1 => "X",
-                    2 => "0",
+                    2 => "O",
                     _ => "?",
                 };
                 print!("{} ", symbol);
             }
             println!();
         }
+    }
+
+    pub fn check_win(&self, x: usize, y: usize) -> bool {
+        //TODO:implement check win
+        false
+
+    }
+
+    pub fn is_board_full(&self) -> bool {
+        for row in self.board.iter() {
+            for cell in row.iter() {
+                if *cell == 0 {
+                    return false;
+                }
+            }
+        }
+        true
     }
 }
